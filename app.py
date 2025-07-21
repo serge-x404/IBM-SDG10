253823af5a3282364fb1afd2d768beef10fa4775
serge-x404 <149474011+serge-x404@users.noreply.github.com>
2025-07-21 21:10:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 import os
 
-api_key = st.secrets["AIzaSyArSp6dRABufUVpJ3qy3KXpE1R7ort4F7I"]
+api_key = st.secrets["general"]["AIzaSyArSp6dRABufUVpJ3qy3KXpE1R7ort4F7I"]
 genai.configure(api_key=api_key)
 
 
